5d99632791a224fcfadcf492ef41b42a018fc169
Rebecanascimentoo <rebequinhayuu@gmail.com>
2025-11-28 20:36:55 -0300

    Ultimo commit

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Biblioteca biblioteca = new Biblioteca();
         Scanner scanner = new Scanner(System.in);
